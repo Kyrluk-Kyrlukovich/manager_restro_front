440289e73b4288cf5af7e3248b1a722b7bd3ad66
Александр <aleksandr@MacBook-Air-1111.local>
2024-03-01 19:50:50 +0500

    csrf-cookie for server

--- src/api/login.ts
+++ src/api/login.ts
@@ -5,7 +5,7 @@ import type { dtoTypes } from "@/types";
 
 export function login(dto: dtoTypes) {
 	return axios
-		.get("http://localhost:80/sanctum/csrf-cookie")
+		.get("http://94.26.225.206:81/sanctum/csrf-cookie")
 		.then((r) => apiInstance.post("/login", dto));
 }
 
