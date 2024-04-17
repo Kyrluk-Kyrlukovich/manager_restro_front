522177277328e1b0cbc4ca7c3f97798881efc22c
Александр <aleksandr@MacBook-Air-1111.local>
2024-04-17 19:58:52 +0500

    fix image

--- src/shared/getServerImage.ts
+++ src/shared/getServerImage.ts
@@ -1,5 +1,5 @@
 export function setUrl(url) {
-    const publicUrl = /public/gi;
-    const slicedUrl = url.replace(publicUrl, 'storage')
-    return `http://localhost:80/${slicedUrl}`;
+	const publicUrl = /public/gi;
+	const slicedUrl = url.replace(publicUrl, "storage");
+	return `http://94.26.255.206:81/${slicedUrl}`;
 }
