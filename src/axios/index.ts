6fd9980507113b10452c5d1b366bb7c8cfc39af3
Александр <aleksandr@MacBook-Air-1111.local>
2024-06-07 18:25:34 +0500

    delete this

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 export const apiInstance = axios.create({
-	baseURL: "http://94.26.225.206:81/api/",
+	baseURL: "http://localhost:80/api",
 });
 
 apiInstance.defaults.withCredentials = true;
